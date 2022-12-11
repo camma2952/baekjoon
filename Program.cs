d17e31eb514fb56b3fca9513beeb1708f24e8c90
camma2952 <camma2952@gmail.com>
2022-12-11 23:31:58 +0900

    v1.4

--- Program.cs
+++ Program.cs
@@ -1,48 +1,65 @@
 ﻿#pragma warning disable
 using System;
-using System.Reflection;
 
 class program
 {
+    static bool checkpr(int n)
+    {
+        if (n == 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    static bool[] ert(int n)
+    {
+        bool[] c = Enumerable.Repeat<bool>(false, n + 5).ToArray<bool>();
+        c[1] = true;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (c[i] == false)
+            {
+                for (int j = i + i; j <= n; j = j + i)
+                {
+                    c[j] = true;
+                }
+            }
+        }
+
+        return c;
+    }
+
     static void Main()
     {
-        static string rs(string str)
+        while (true)
         {
-            char[] arr = str.ToCharArray();
-            Array.Reverse(arr);
-            return new string(arr);
-        }
+            int n = int.Parse(Console.ReadLine()), cnt = 0;
+            bool[] prime = ert(n);
 
-        string[] n = Console.ReadLine().Split();
-        string a = rs(n[0]);
-        string b = rs(n[1]);
-        string res = "";
-        int len = Math.Max(a.Length, b.Length), up = 0;
-
-        for(int i = 0; i < len; i++)
-        {
-            int ap = i < a.Length ? a[i] - '0' : 0;
-            int bp = i < b.Length ? b[i] - '0' : 0;
-            int sum = ap + bp + up;
-
-            if (sum >= 10)
+            if (n == 0)
             {
-                up = 1;
-                sum -= 10;
-            }
-            else
-            {
-                up = 0;
+                break;
             }
 
-            res += sum.ToString();
-        }
+            for(int i = n; i <= n * 2; i++)
+            {
+                if (prime[i])
+                {
+                    cnt++;
+                }
+            }
 
-        if (up == 1)
-        {
-            res += "1";
+            Console.WriteLine(cnt);
         }
-
-        Console.WriteLine(rs(res));
     }
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ v1.1: visual studio git 테스트
 v1.2: saves 폴더 업데이트
 
 v1.3: saves 폴더 업데이트
+
+v1.4: saves 폴더 업데이트
